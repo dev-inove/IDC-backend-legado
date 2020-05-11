07debed0e2bb657063497f30baae7ba369a01a45
lunebakami <luis.santos.taiba@gmail.com>
2020-05-11 19:41:05 -0300

    database configured

---
+++ src/database/index.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+class Database {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    try {
+      this.mongoConnection = mongoose.connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useFindAndModify: true,
+        useUnifiedTopology: true,
+      });
+
+      console.log('Database connected!');
+    } catch (e) {
+      console.log("Database didn't connect!");
+    }
+  }
+}
+
+module.exports = new Database();
