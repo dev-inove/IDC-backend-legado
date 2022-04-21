65101d74d80a7ad1ece62831913abd4af02c717a
Bruno Lopes <eusoubruno7@gmail.com>
2022-04-21 13:35:51 -0300

    fix: reorganizando os controllers e adicionando o jsconfig

--- src/app/config/moduleAliasConfig.js
+++ src/app/config/moduleAliasConfig.js
@@ -2,7 +2,7 @@ const moduleAlias = require('module-alias');
 const path = require('path');
 
 moduleAlias.addAliases({
-  '~': path.join(__dirname, '..'),
+  '~': path.join(__dirname, '..', '..', '..'),
   '@models': path.join(__dirname, '..', 'models'),
   '@controllers': path.join(__dirname, '..', 'controllers'),
   '@config': path.join(__dirname),
