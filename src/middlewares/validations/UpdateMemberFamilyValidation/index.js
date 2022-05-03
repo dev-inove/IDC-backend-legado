82a9389c2870783f35ed18b1c118b24ed1c3f1fa
Bruno Lopes <eusoubruno7@gmail.com>
2022-05-03 14:14:56 -0300

    fix: corrigindo o middleware de validação da atualização de menbro da família

--- src/middlewares/validations/UpdateMemberFamilyValidation/index.js
+++ src/middlewares/validations/UpdateMemberFamilyValidation/index.js
@@ -2,29 +2,49 @@ const Yup = require('yup');
 
 async function UpdateMemberFamilyValidation(request, response, next) {
   const schema = Yup.object().shape({
-    CPFAssisted: Yup.string(),
-    kinship: Yup.string(),
-    name: Yup.string(),
-    rg: Yup.string(),
-    cpf: Yup.string(),
-    fones: Yup.array().of(Yup.number()),
-    email: Yup.string(),
-    renda: Yup.number(),
-    isResponsible: Yup.boolean(),
+    CPFAssisted: Yup.string().typeError(
+      'O CPF do assistido deve ser um texto.',
+    ),
+    kinship: Yup.string().typeError('O parentesco deve ser um texto.'),
+    name: Yup.string().typeError('O nome deve ser um texto.'),
+    rg: Yup.string().typeError('O rg deve ser um texto.'),
+    cpf: Yup.string().typeError('o cpf deve ser um texto.'),
+    fones: Yup.array().of(
+      Yup.number().typeError('O telefone deve ser um número.'),
+    ),
+    email: Yup.string().typeError('O email deve ser umm texto.'),
+    renda: Yup.number().typeError('A renda deve ser um número.'),
+    isResponsible: Yup.boolean().typeError(
+      'A responsabilidade deve ser um boolean.',
+    ),
     responsible: Yup.object().shape({
-      rg: Yup.string(),
-      responsibleValidator: Yup.string(),
-      organization: Yup.string(),
-      validity: Yup.string(),
+      rg: Yup.string().typeError('O rg do responsável deve ser um texto.'),
+      responsibleValidator: Yup.string().typeError(
+        'A validação de responsável deve ser um texto.',
+      ),
+      organization: Yup.string().typeError('A organização deve ser um texto.'),
+      validity: Yup.string().typeError('A validade deve ser um texto.'),
     }),
-    wasAttended: Yup.boolean(),
-    doMedicalTreatment: Yup.boolean(),
-    useContinuosMedication: Yup.boolean(),
-    typeOfDisiase: Yup.string(),
+    wasAttended: Yup.boolean().typeError(
+      'Se foi atendido deve ser um boolean.',
+    ),
+    doMedicalTreatment: Yup.boolean().typeError(
+      'Se está fazendo tratamento médico deve ser um boolean.',
+    ),
+    useContinuosMedication: Yup.boolean().typeError(
+      'Se faz uso constante de medicamento deve ser um boolean.',
+    ),
+    typeOfDisiase: Yup.string().typeError(
+      'O tipo da doença deve ser um texto.',
+    ),
   });
 
-  if (!(await schema.isValid(request.body, { strict: true }))) {
-    return response.status(400).json({ message: 'Invalid Object' });
+  try {
+    await schema.validate(request.body, { strict: true, abortEarly: false });
+  } catch (err) {
+    const validationErros = err.errors;
+
+    return response.status(400).json({ errors: validationErros });
   }
 
   return next();
