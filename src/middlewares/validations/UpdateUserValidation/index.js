4a6318cffd91818c9898dcffbbd4500b7acc8e6a
Bruno Lopes <eusoubruno7@gmail.com>
2022-05-03 13:44:03 -0300

    fix: corrigindo o middleware de validação da atualização de usuário

--- src/middlewares/validations/UpdateUserValidation/index.js
+++ src/middlewares/validations/UpdateUserValidation/index.js
@@ -2,15 +2,25 @@ const Yup = require('yup');
 
 async function UpdateUserValidation(request, response, next) {
   const schema = Yup.object().shape({
-    name: Yup.string(),
-    email: Yup.string().required(),
-    password: Yup.string().required(),
-    newPassword: Yup.string(),
-    confirmNewPassword: Yup.string(),
+    name: Yup.string().typeError('O nome deve ser um texto.'),
+    email: Yup.string()
+      .required('O email deve ser informado.')
+      .typeError('O email deve ser um texto.'),
+    password: Yup.string()
+      .required('A senha deve ser informada.')
+      .typeError('A senha deve ser um texto.'),
+    newPassword: Yup.string().typeError('A nova senha deve ser um texto.'),
+    confirmNewPassword: Yup.string().typeError(
+      'A confirmação da nova senha deve ser um texto.',
+    ),
   });
 
-  if (!(await schema.isValid(request.body, { strict: true }))) {
-    return response.status(400).json({ message: 'Format invalid' });
+  try {
+    await schema.validate(request.body, { strict: true, abortEarly: false });
+  } catch (err) {
+    const validationErros = err.errors;
+
+    return response.status(400).json({ errors: validationErros });
   }
 
   return next();
