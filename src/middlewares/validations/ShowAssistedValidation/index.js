e06aaaedbc9b3ff52899608d671e576810468d71
Thiago Viana Bezerra <Thiagovb62@gmail.com>
2022-05-04 13:46:31 -0300

    atualizando a ortagrafia do middleware de ShowAssistedValidation.

--- src/middlewares/validations/ShowAssistedValidation/index.js
+++ src/middlewares/validations/ShowAssistedValidation/index.js
@@ -4,7 +4,7 @@ async function ShowAssistedValidation(request, response, next) {
   const schema = Yup.object().shape({
     id: Yup.string()
       .required('O id do assistido deve ser informado.')
-      .typeError('O id do assistido deve ser um texto'),
+      .typeError('O id do assistido deve ser um texto.'),
   });
 
   try {
