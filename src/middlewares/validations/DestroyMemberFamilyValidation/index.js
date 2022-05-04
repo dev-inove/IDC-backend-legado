a133278dc34f1f34f76262dcb8e18f87fd81a64b
Thiago Viana Bezerra <Thiagovb62@gmail.com>
2022-05-04 09:22:39 -0300

    fix:corrigindo o middleware validaçao da destruiçao do membro da familia.

--- src/middlewares/validations/DestroyMemberFamilyValidation/index.js
+++ src/middlewares/validations/DestroyMemberFamilyValidation/index.js
@@ -2,13 +2,21 @@ const Yup = require('yup');
 
 async function DestroyMemberFamilyValidation(request, response, next) {
   const schema = Yup.object().shape({
-    id: Yup.string().required(),
+    id: Yup.string()
+      .required('o id deve ser informado')
+      .typeError('o id deve ser um texto'),
   });
 
-  if (!(await schema.isValid(request.params, { strict: true }))) {
-    return response.status(400).json({ message: 'Invalid Id!' });
-  }
+  try {
+    await schema.validate(request.body, {
+      strict: true,
+      abortEarly: false,
+    });
+  } catch (err) {
+    const validationErros = err.errors;
 
+    return response.status(400).json({ errors: validationErros });
+  }
   return next();
 }
 
