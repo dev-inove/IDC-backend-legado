f1c4d5ff5c5abb3f7be64b98aef3cb464b8eeeb1
Thiago Viana Bezerra <Thiagovb62@gmail.com>
2022-05-04 09:35:27 -0300

    fix:corrigindo ortografia do middleware de CreateUserValidation.

--- src/middlewares/validations/CreateUserValidation/index.js
+++ src/middlewares/validations/CreateUserValidation/index.js
@@ -3,14 +3,14 @@ const Yup = require('yup');
 async function CreateUserValidation(request, response, next) {
   const schema = Yup.object().shape({
     name: Yup.string()
-      .required('O nome deve ser informado')
-      .typeError('O nome deve ser um texto'),
+      .required('O nome deve ser informado.')
+      .typeError('O nome deve ser um texto.'),
     email: Yup.string()
-      .required('O email deve ser informado')
-      .typeError('O email deve ser um texto'),
+      .required('O email deve ser informado.')
+      .typeError('O email deve ser um texto.'),
     password: Yup.string()
-      .required('A senha deve ser informado')
-      .typeError('A senha deve ser um texto'),
+      .required('A senha deve ser informado.')
+      .typeError('A senha deve ser um texto.'),
   });
 
   try {
