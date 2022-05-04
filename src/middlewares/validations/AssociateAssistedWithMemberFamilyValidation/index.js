e03f6635b0e1c725022acfa8d46030546fc5103c
Thiago Viana Bezerra <Thiagovb62@gmail.com>
2022-05-04 13:36:33 -0300

    atualizando o middleware de associateAssistedWithMemberFamilyValidation.

--- src/middlewares/validations/AssociateAssistedWithMemberFamilyValidation/index.js
+++ src/middlewares/validations/AssociateAssistedWithMemberFamilyValidation/index.js
@@ -6,15 +6,27 @@ async function AssociateAssistedWithMemberFamilyValidation(
   next,
 ) {
   const schema = Yup.object().shape({
-    CPFAssisted: Yup.string().required(),
-    CPFMemberFamily: Yup.string().required(),
-    isResponsible: Yup.boolean().required(),
+    CPFAssisted: Yup.string()
+      .required('O cpf do assistido deve ser informado.')
+      .typeError('O cpf do assistido deve ser um texto.'),
+    CPFMemberFamily: Yup.string()
+      .required('Cpf do membro da familia deve ser informado.')
+      .typeError('Cpf do membro da familia deve ser um texto'),
+    isResponsible: Yup.boolean()
+      .required('Se é o responsável legal deve ser informado.')
+      .typeError('Se é o responsável legal deve ser verdadeiro ou falso.'),
   });
 
-  if (!(await schema.isValid(request.body, { strict: true }))) {
-    return response.status(400).json({ message: 'validations fails' });
-  }
+  try {
+    await schema.validate(request.body, {
+      strict: true,
+      abortEarly: false,
+    });
+  } catch (err) {
+    const validationErros = err.errors;
 
+    return response.status(400).json({ error: validationErros });
+  }
   return next();
 }
 
