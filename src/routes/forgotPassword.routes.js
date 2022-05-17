2534403e1fbd4f474fdf0751b8294ac524a20bfb
Bruno Lopes <eusoubruno7@gmail.com>
2022-05-16 22:20:29 -0300

    feat: adicionando alteração de senha utilizando o token enviado para o email do usuário

--- src/routes/forgotPassword.routes.js
+++ src/routes/forgotPassword.routes.js
@@ -1,8 +1,13 @@
 const FoorgotPasswordController = require('@controllers/FoorgotPasswordController');
+const UpdateUserPasswordWithTokenController = require('@controllers/UpdateUserPasswordWithTokenController');
 const { Router } = require('express');
 
 const forgotPasswordRouter = Router();
 
 forgotPasswordRouter.post('/', FoorgotPasswordController.store);
+forgotPasswordRouter.post(
+  '/updatepassword',
+  UpdateUserPasswordWithTokenController.update,
+);
 
 module.exports = forgotPasswordRouter;
