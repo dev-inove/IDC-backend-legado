31cd4b546b78dd7e9c866293a0850fca519e8933
brunoapollon <eusoubruno7@gmail.com>
2022-05-20 14:45:55 -0300

    feat: criando service de criação de menbros da família

--- src/controllers/MemberFamilyController/index.js
+++ src/controllers/MemberFamilyController/index.js
@@ -3,82 +3,64 @@ const Assisted = require('@models/AssistedUser');
 
 const ReturnByTypeVariableAndEdit = require('@service/ReturnAllMemberFamilyByTypeService');
 const ReturnByTypeVariable = require('@service/ReturnMemberFamilyByTypeService');
+const CreateMemberFamilyService = require('@service/CreateMemberFamilyService');
 
 class MemberFamilyController {
-  async store(req, res) {
-    const { cpf_assisted } = req.body;
-
-    const memberFamily = new MemberFamily(req.body);
-
-    const assistedUser = await Assisted.findOne({ cpf: cpf_assisted });
-
-    if (memberFamily.isResponsible === true) {
-      if (!assistedUser) {
-        return res.status(400).json({
-          message: 'User not found, try again with another CPF',
-        });
-      }
-
-      if (
-        assistedUser.id_Responsible !== undefined &&
-        assistedUser.id_Responsible !== null
-      ) {
-        return res.status(401).json('This Assisted already has a Responsible');
-      }
-
-      assistedUser.id_Responsible = memberFamily._id;
-      memberFamily.idAssisted.push(assistedUser._id);
-      assistedUser.save();
-    }
-
-    if (memberFamily.idAssisted.length === 0) {
-      memberFamily.idAssisted.push(assistedUser._id);
-    }
-
+  async store(request, response) {
     try {
-      await memberFamily.save();
-    } catch (error) {
-      return res.status(400).json({ message: error });
-    }
+      const memberFamilyData = request.body;
 
-    return res.status(201).json(memberFamily);
+      const createMemberFamilyService = new CreateMemberFamilyService();
+
+      const memberFamilyCreated = await createMemberFamilyService.execute(
+        memberFamilyData,
+      );
+
+      return response.status(201).json(memberFamilyCreated);
+    } catch (err) {
+      const errorMessage = err.message;
+
+      return response.status(400).json({ error: errorMessage });
+    }
   }
 
-  async index(req, res) {
-    const { idAssisted } = req.params;
+  async index(request, response) {
+    const { idAssisted } = request.params;
     const members = await MemberFamily.find({
       idAssisted,
     }).populate('idAssisted', 'fullName');
 
     if (members.length === 0) {
-      return res.status(400).json({
+      return response.status(400).json({
         message: "This user don't have members of family in the system",
       });
     }
-    return res.status(200).json({ members });
+    return response.status(200).json({ members });
   }
 
-  async show(req, res) {
-    const { id } = req.params;
-    const { type } = req.query;
+  async show(request, response) {
+    const { id } = request.params;
+    const { type } = request.query;
 
     const member = await ReturnByTypeVariable.exec(type, id);
 
     if (member === undefined || member === null) {
-      return res.status(400).json({ message: 'Member of Family not found' });
+      return response
+        .status(400)
+        .json({ message: 'Member of Family not found' });
     }
-    return res.status(202).json({ member });
+    return response.status(202).json({ member });
   }
 
-  async update(req, res) {
-    const { id } = req.params;
-    const { type } = req.query;
+  async update(request, response) {
+    const { id } = request.params;
+    const { type } = request.query;
 
-    const isResponsibleBody = req.body.isResponsible;
+    const isResponsibleBody = request.body.isResponsible;
 
     const member = await ReturnByTypeVariableAndEdit.exec(type, id);
 
-    const { CPFAssisted } = req.body;
+    const { CPFAssisted } = request.body;
 
     const assistedUser = await Assisted.findOne({ cpf: CPFAssisted });
 
@@ -90,7 +72,7 @@ class MemberFamilyController {
 
     if (member.isResponsible === false && isResponsibleBody === true) {
       if (!member.idAssisted.includes(assistedUser.id)) {
-        return res.status(401).json({
+        return response.status(401).json({
           message: 'This Assisted and Member are not parents',
         });
       }
@@ -100,29 +82,29 @@ class MemberFamilyController {
       } else if (assistedUser.id_Responsible === null) {
         assistedUser.set('id_Responsible', member._id);
       } else {
-        return res
+        return response
           .status(401)
           .json({ message: 'this user alredy has a Responsible' });
       }
       try {
         await assistedUser.save();
       } catch (error) {
-        return res.status(400).json({ message: error });
+        return response.status(400).json({ message: error });
       }
     }
     try {
-      member.set(req.body);
+      member.set(request.body);
 
       await member.save();
-      return res.status(204).json({ member_updated: member });
+      return response.status(204).json({ member_updated: member });
     } catch (error) {
-      return res.status(400).json({ message: error });
+      return response.status(400).json({ message: error });
     }
   }
 
-  async destroy(req, res) {
-    const { id } = req.params;
-    const { type } = req.query;
+  async destroy(request, response) {
+    const { id } = request.params;
+    const { type } = request.query;
     try {
       const member = await ReturnByTypeVariableAndEdit.exec(type, id);
 
@@ -137,9 +119,11 @@ class MemberFamilyController {
       }
 
       member.remove();
-      return res.status(204).json({ message: 'Member deleted!' });
+      return response.status(204).json({ message: 'Member deleted!' });
     } catch (err) {
-      return res.status(401).json({ message: `Member not deleted! ${err}` });
+      return response
+        .status(401)
+        .json({ message: `Member not deleted! ${err}` });
     }
   }
 }
---
+++ src/service/CreateMemberFamilyService/index.js
@@ -0,0 +1,43 @@
+const MemberFamily = require('@models/MemberFamily');
+const AssistedUser = require('@models/AssistedUser');
+
+class CreateMemberFamilyService {
+  async execute(memberFamilyData) {
+    const { cpf_assisted } = memberFamilyData;
+
+    const memberFamily = new MemberFamily(memberFamilyData);
+
+    const assistedUser = await AssistedUser.findOne({ cpf: cpf_assisted });
+
+    if (memberFamily.isResponsible === true) {
+      if (!assistedUser) {
+        throw new Error(
+          'Assistido não encontrado, verifique o cpf e tente novamente!',
+        );
+      }
+
+      if (
+        assistedUser.id_Responsible !== undefined &&
+        assistedUser.id_Responsible !== null
+      ) {
+        throw new Error(
+          'O assistido já possui um responsável, desmarque a opção de responsável e cadastre o menbro novamente!',
+        );
+      }
+
+      assistedUser.id_Responsible = memberFamily._id;
+      memberFamily.idAssisted.push(assistedUser._id);
+      await assistedUser.save();
+    }
+
+    if (memberFamily.idAssisted.length === 0) {
+      memberFamily.idAssisted.push(assistedUser._id);
+    }
+
+    await memberFamily.save();
+
+    return memberFamily;
+  }
+}
+
+module.exports = CreateMemberFamilyService;
