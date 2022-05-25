2d13b49401d1bcb856e770dae3d61af7ee467042
Bruno Lopes <eusoubruno7@gmail.com>
2022-05-24 21:32:09 -0300

    feat: adicionando middleware de validação da rota forgotPassword

---
+++ src/middlewares/validations/FoorgotPasswordValidation/index.js
@@ -0,0 +1,21 @@
+const Yup = require('yup');
+
+async function FoorgotPasswordValidation(request, response, next) {
+  const schema = Yup.object().shape({
+    email: Yup.string()
+      .required('O email é obrigatório para solicitar a alteração de senha.')
+      .typeError('O email deve ser um texto.'),
+  });
+
+  try {
+    await schema.validate(request.body, { strict: true, abortEarly: false });
+  } catch (err) {
+    const validationErros = err.errors;
+
+    return response.status(400).json({ errors: validationErros });
+  }
+
+  return next();
+}
+
+module.exports = FoorgotPasswordValidation;
--- src/routes/forgotPassword.routes.js
+++ src/routes/forgotPassword.routes.js
@@ -1,10 +1,15 @@
 const FoorgotPasswordController = require('@controllers/FoorgotPasswordController');
 const UpdateUserPasswordWithTokenController = require('@controllers/UpdateUserPasswordWithTokenController');
+const FoorgotPasswordValidation = require('@middlewares/validations/FoorgotPasswordValidation');
 const { Router } = require('express');
 
 const forgotPasswordRouter = Router();
 
-forgotPasswordRouter.post('/', FoorgotPasswordController.store);
+forgotPasswordRouter.post(
+  '/',
+  FoorgotPasswordValidation,
+  FoorgotPasswordController.store,
+);
 forgotPasswordRouter.post(
   '/updatepassword',
   UpdateUserPasswordWithTokenController.update,
