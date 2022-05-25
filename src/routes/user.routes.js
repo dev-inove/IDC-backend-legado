ea529fcb356269903c560e15f56a5e6ee1eb0977
Bruno Lopes <eusoubruno7@gmail.com>
2022-05-25 15:30:46 -0300

    feat: adicionando middleware de validação na visualização de usuário

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,13 +1,14 @@
 const { Router } = require('express');
-const UserController = require('@controllers/UserController');
-
 const passport = require('passport');
+
+const UserController = require('@controllers/UserController');
+const UpdateUserPasswordController = require('@controllers/UpdateUserPasswordController');
 
 const UpdateUserValidation = require('@middlewares/validations/UpdateUserValidation');
 const CreateUserValidation = require('@middlewares/validations/CreateUserValidation');
-const SchemaPassport = require('@middlewares/Auth');
 const UpdateUserPasswordValidation = require('@middlewares/validations/UpdateUserPasswordValidation');
-const UpdateUserPasswordController = require('@controllers/UpdateUserPasswordController');
+const ShowUserValidation = require('@middlewares/validations/ShowUserValidation');
+const SchemaPassport = require('@middlewares/Auth');
 
 const userRouter = Router();
 
@@ -16,7 +17,7 @@ userRouter.post('/', CreateUserValidation, UserController.store);
 passport.use(SchemaPassport);
 userRouter.use(passport.authenticate('jwt', { session: false }));
 
-userRouter.get('/:email', UserController.show);
+userRouter.get('/:email', ShowUserValidation, UserController.show);
 userRouter.get('/', UserController.index);
 userRouter.put('/', UpdateUserValidation, UserController.update);
 userRouter.patch(
