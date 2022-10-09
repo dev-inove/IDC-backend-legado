087cd0dc1fd16ffb7e73b3cdaf883d83c324f4bb
Juandbpimentel <juandbpimentel@alu.ufc.br>
2022-10-08 23:27:17 -0300

    Feat: Atualizações nas variáveis de ambiente para nova estrutura do projeto

--- src/config/moduleAliasConfig.js
+++ src/config/moduleAliasConfig.js
@@ -4,9 +4,5 @@ const path = require('path');
 module.exports = moduleAlias.addAliases({
   '@modules':path.join(__dirname, '..', 'modules'),
   '@shared':path.join(__dirname, '..', 'shared'),
-  '@models': path.join(__dirname, '..', 'models'),
-  '@controllers': path.join(__dirname, '..', 'controllers'),
   '@config': path.join(__dirname),
-  '@service': path.join(__dirname, '..', 'service'),
-  '@routes': path.join(__dirname, '..', 'routes'),
 });
