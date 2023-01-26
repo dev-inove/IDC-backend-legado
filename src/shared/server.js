f12f393eaf715988233f8f02462b0418e4d8a4e7
Juan David Bizerra Pimentel <juandbpimentel@alu.ufc.br>
2023-01-25 23:07:04 -0300

    Feat - Adicionando variável de ambiente para port do servidor

--- src/shared/server.js
+++ src/shared/server.js
@@ -1,5 +1,5 @@
 const server = require('./app');
 
 server.listen(process.env.SERVER_PORT,()=>{
-    console.log(`Server ativo na porta ${process.env.SERVER_PORT}`);
+    console.log(`O servidor está ativo na porta ${process.env.SERVER_PORT}!`);
 });
